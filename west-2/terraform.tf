0b1d946c3939bc1839f301e09398987d08754e66
faustomontiel <faustomontiel98@outlook.com>
2025-04-25 23:55:01 -0300

    names fixes

--- west-2/terraform.tf
+++ west-2/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "transacctionsprods-terraform-state"
     key = "west-1/terraform.tfstate"
-    region = "us-west-1" 
+    region = "us-west-2" 
     dynamodb_table = "terraform-locks"
   }
 }
