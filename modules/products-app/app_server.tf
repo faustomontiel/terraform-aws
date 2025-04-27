706ddedfc8ab93e7e15f6f740aa73754b1fd4854
faustomontiel <faustomontiel98@outlook.com>
2025-04-27 19:12:48 -0300

    ssh key

--- modules/products-app/app_server.tf
+++ modules/products-app/app_server.tf
@@ -1,6 +1,10 @@
 resource "aws_instance" "ec2_transactions" {
   ami = var.ami
   instance_type = var.instance_type
+  
+  key_name                    = aws_key_pair.web.key_name
+  vpc_security_group_ids      = [aws_security_group.ssh_access.id]
+  
   tags = {
     Name = "${var.app_region}-app-server"
   }
@@ -8,3 +12,28 @@ resource "aws_instance" "ec2_transactions" {
                 aws_s3_bucket.transacction_data]
 
 }
+resource "aws_key_pair" "web" {
+  key_name   = "web-key-ec2"
+  public_key = file("~/.ssh/web-key.pub")
+}
+
+resource "aws_security_group" "ssh_access" {
+  name        = "ssh-access-ec2"
+  description = "Allow SSH access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
--- west-2/terraform.tf
+++ west-2/terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "transacctionsprods-terraform-state"
-    key = "west-1/terraform.tfstate"
-    region = "us-west-2" 
+    key = "west-2/terraform.tfstate"
+    region = "us-west-1" 
     dynamodb_table = "terraform-locks"
   }
 }
